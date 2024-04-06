ff23ef9d2a8a16b9815e1b05c7e15c7b6c9ecbea
malcolm <malcolmboyd24@gmail.com>
2024-04-06 17:03:45 -0300

    added comments lol

--- Chudnovsky.go
+++ Chudnovsky.go
@@ -14,54 +14,86 @@ var wg = sync.WaitGroup{}
 // channel is used to send information between goroutines
 var channel = make(chan *big.Float, iterations)
 
+// input is desired_decimals i.e. ./Chudnovsky 100 will print out 100 decimals
 var desired_decimals, _ = strconv.Atoi(os.Args[1])
+
+// each iteration makes minimum 14 decimals
 var iterations = desired_decimals / 14
+
+// this is just a number that seems to work
+// may be able to lower it for speedup in some situations (not sure)
 var precision = iterations * 50
+
+// const1 is just a value used in each sum
+// global variable so we don't have to calculate it everytime
 var const1 = big.NewInt(13_591_409)
 
 func main() {
+	// sum is the sum which we are parallelizing
 	sum := new(big.Float).SetPrec(uint(precision))
 
 	for i := 0; i < iterations; i++ {
+		// add a goroutine to the waitgroup
+		// and call the go routine
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
 			chudnovsky(i)
 		}(i)
-	}
 
-	wg.Wait()
-	for i := 0; i < iterations; i++ {
+		// add to the sum from that goroutine (cannot be done in parallel bc of race conditions)
 		sum.Add(sum, <-channel)
+
+		// occationally print out how many digits we have so far
+		if i%1000 == 0 {
+			numerator := big.NewFloat(10_005).SetPrec(uint(precision))
+			numerator.Sqrt(numerator)
+			numerator.Mul(numerator, big.NewFloat(426_880))
+			pi := numerator.Quo(numerator, sum)
+
+			fmt.Println(pi)
+			fmt.Println()
+		}
 	}
+	// close the channel
 	close(channel)
 
+	fmt.Println("Final:")
+	fmt.Println()
 	numerator := big.NewFloat(10_005).SetPrec(uint(precision))
 	numerator.Sqrt(numerator)
 	numerator.Mul(numerator, big.NewFloat(426_880))
+
 	pi := numerator.Quo(numerator, sum)
 
 	fmt.Println(pi)
 }
 
 func chudnovsky(k int) {
-
+	// start with big.Ints because ints are faster and they are all integers in the fraction
 	temp := big.NewInt(545_140_134)
 	temp.Mul(temp, big.NewInt(int64(k)))
 	temp.Add(temp, const1)
 
+	// numerator of the sum
+	// (6k)!(545,140,134k + 13,591,409)
 	int_numerator := new(big.Int)
 	int_numerator = int_numerator.Mul(factorial(6*k), temp)
 
+	// denominator of the sum
+	// (3k)!(k!)^3(640320)^3k
 	int_denominator := new(big.Int)
 	int_denominator.Mul(factorial(3*k), new(big.Int).Exp(factorial(k), big.NewInt(3), nil))
 	int_denominator.Mul(int_denominator, new(big.Int).Exp(big.NewInt(640_320), big.NewInt(int64(3*k)), nil))
 
+	// then convert to floats with the required precision
 	denominator := new(big.Float).SetInt(int_denominator).SetPrec(uint(precision))
 	numerator := new(big.Float).SetInt(int_numerator).SetPrec(uint(precision))
 
+	// divide them
 	denominator.Quo(numerator, denominator)
 
+	// return the result to the channel
 	if k%2 == 0 {
 		channel <- denominator
 	} else {
@@ -69,6 +101,7 @@ func chudnovsky(k int) {
 	}
 }
 
+// pretty fuckin self explanatory
 func factorial(n int) *big.Int {
 	if n <= 1 {
 		return big.NewInt(1)
